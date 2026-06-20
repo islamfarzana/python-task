8ef317de019fa19601ace69a5a180c2423a68fdb
Farzana Islam <islamfarzana124@gmail.com>
2026-06-20 19:21:37 +0600

    Task 1: Variables and Data Types

---
+++ task_01_variables.py
@@ -0,0 +1,9 @@
+name = "Shadik"         
+age = 25                
+height = 5.9           
+is_student = True       
+
+print("Name:", name)
+print("Age:", age)  
+print("Height:", height)
+print("Is Student:", is_student)
